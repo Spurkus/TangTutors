cf11a962e50bd696e13fa5f330ae1ed7358ae445
Spurkus <89559431+Spurkus@users.noreply.github.com>
2023-11-15 12:53:45 +1000

    Footer for now...
    adding footer into website

---
+++ app/components/Footer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faMedium,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">Home</a>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact Us</a>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <a>
+            <FontAwesomeIcon icon={faLinkedin} className="h-7 w-7" />
+          </a>
+          <a>
+            <FontAwesomeIcon icon={faGithub} className="h-7 w-7" />
+          </a>
+          <a>
+            <FontAwesomeIcon icon={faMedium} className="h-7 w-7" />
+          </a>
+        </div>
+      </nav>
+      <aside>
+        <p>A personal tutoring website made just for you :)</p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
