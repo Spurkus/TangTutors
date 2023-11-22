823bf458a748d71744f57dd7bd8536cc5231e348
Spurkus <89559431+Spurkus@users.noreply.github.com>
2023-11-22 14:05:09 +1000

    Fixed icon for dark mode and light mode
    yay

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,26 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
+import logo from "public/logo.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "TangTutors",
-  description: "A personal high school tutoring website based in Australia",
+  description:
+    "This is a high school tutoring website for those who are in need of help in QCE and the QCAA curriculum. Here at Tang Tutors, we are committed to helping and educating every student with personalised tutoring sessions. A personal high school tutoring website based in Australia",
+  icons: [
+    {
+      rel: "icon",
+      url: "/logo.png",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      rel: "icon",
+      url: "/logo2.png",
+      media: "(prefers-color-scheme: dark)",
+    },
+  ],
 };
 
 export default function RootLayout({
